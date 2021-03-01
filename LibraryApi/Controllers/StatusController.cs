3d36baab17f81b22a0f0385da16d048cffdaad24
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-01 14:41:56 -0500

    Update StatusController.cs

--- LibraryApi/Controllers/StatusController.cs
+++ LibraryApi/Controllers/StatusController.cs
@@ -8,11 +8,37 @@ namespace LibraryApi.Controllers
 {
     public class StatusController : ControllerBase
     {
+
         // GET /status
         [HttpGet("status")]
-        public string Getstatus()
+        public StatusResponse GetTheStatus()
         {
-            return "Looks good!";
+            return new StatusResponse
+            {
+                Message = "Everything is going great. Thanks for asking!",
+                LastChecked = DateTime.Now
+            };
+        }
+
+
+        [HttpGet("cutomers/{customerId:int}")]
+        public ActionResult GetInfoAboutCustomer(int customerId)
+        {
+            return Ok($"Getting info about customer {customerId}");
+        }
+
+        [HttpGet("blogs/{year:int}/{month:int}/{day:int}")]
+        public ActionResult GetBlogPost(int year, int month, int day)
+        {
+            return Ok($"Getting info about blogs at {day}/{month}/{year}");
         }
     }
+
+    public class StatusResponse
+    {
+        public string Message { get; set; }
+        public DateTime LastChecked { get; set; }
+    }
+
+
 }
