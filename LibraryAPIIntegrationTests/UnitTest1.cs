0f91afda69071ef3213f623b13b2f54933f70e66
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-01 16:35:10 -0500

    Created test project

---
+++ LibraryAPIIntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibraryAPIIntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
