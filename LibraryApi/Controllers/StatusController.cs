95f1fb2545882c3692f18c918d6850c796a8a56b
Ryan Yoak <ryan.yoak@gmail.com>
2021-03-01 13:56:37 -0500

    First commit!

---
+++ LibraryApi/Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        // GET /status
+        [HttpGet("status")]
+        public string Getstatus()
+        {
+            return "Looks good!";
+        }
+    }
+}
